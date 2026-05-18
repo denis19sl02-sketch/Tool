44f84565317288c6703c95b9583a6812396fedb9
Denis Shenel <denis19sl02@gmail.com>
2026-05-18 06:38:32 +0300

    Delete 1.js

--- 1.js
+++
@@ -1,8 +0,0 @@
-// Логика AND, OR, NOT
-function calculateBasicLogic(op, a, b) {
-    if (op === 'NOT') return !a ? 1 : 0;
-    if (op === 'AND') return (a && b) ? 1 : 0;
-    if (op === 'OR')  return (a || b) ? 1 : 0;
-    return 0;
-}
-console.log("python.1 загружен");
